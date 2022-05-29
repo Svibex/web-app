d3a85d289d7b3b6a06d849d1ad6dc2da2093a9a2
Svibex <future.science@yandex.ru>
2022-05-29 21:26:08 +0300

    feat: update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-      <HashRouter>
+      <BrowserRouter>
           <App />
-      </HashRouter>
+      </BrowserRouter>
   </React.StrictMode>
 );
